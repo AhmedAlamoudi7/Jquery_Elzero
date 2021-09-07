f1ef7526d0fb417d3daabfbd1e353213a9f7afb5
Eng.Ahmed ALAmoudi <76726399+AhmedAlamoudi7@users.noreply.github.com>
2021-09-07 20:06:40 +0300

    Adding Index2 style to File

---
+++ js/Plugin.js
@@ -0,0 +1,235 @@
+ $(document).ready(function () {
+
+
+
+     //     //  start css  
+     //     $("button").dblclick(function () {
+
+     //         $("p").css("color", "red");
+     //     });
+     //     //  end css
+
+
+
+
+     //     //  start click 
+     //     $("button").click(function () {
+
+     //         $("p").css("color", "red");
+     //     });
+     //     //  end click 
+
+
+
+
+     //     //  start double click 
+     //     $("button").dblclick(function () {
+
+     //         $("p").css("color", "red");
+     //     });
+     //     //  end double click 
+
+
+
+     //     //  start mouseenter  
+     //     $("p").mouseenter(function () {
+     //         // this == "p"
+     //         $(this).css("color", "yellow");
+     //     });
+     //     //  end mouseenter 
+
+
+
+     //     //  start mouseleave  
+     //     $("p").mouseleave(function () {
+     //         // this == "p"
+
+     //         $(this).css("color", "blue");
+     //     });
+     //     //  end mouseenter 
+
+
+     //  start hover  
+     // in hover you should put to function like mouseenter mouseleave
+     // hover(function () { //code}, function (//code) { });
+     //      $("p").hover(function () {
+     //          $(this).css("color", "orange");
+     //     },
+     //     function () {
+     //         $(this).css("color", "black");
+     // });
+     //     //  end hover 
+
+
+     // //Start show
+     //     $("p").hide();
+     //  $("button").click(function () {
+     //      $("p").show();
+     //  });
+     // // End Show 
+
+
+     //Start toggle
+     //   عامل زي مفتاح المهرباء لو ضغطت مرة يظهر لو ممان مرة يطفي
+     //      $("p").hide();
+     //  $("button").click(function () {
+     //         // toggle(speed, callback)
+     //      $("p").toggle(2000);
+     //      });
+     //      // End Show 
+
+
+
+     //  start hide click 
+     //  hide(speed,callback)
+     //    $("button").hide();
+
+     // $("button").click(function () {
+     //     // toggle(speed, callback)
+     //     $("p").hide(1000, function () {
+     //         $("span").show();
+     //     });
+     // });
+     //  end hide click 
+
+     // Start FadeOut
+     //  $("button").click(function () {
+     //      // toggle(speed, callback)
+     //      $("div").fadeOut(1000, function () {
+     //          $("div").css("display", "block");
+     //      });
+     //  });
+
+
+     //  End FadeOut
+     //  start FadeIn
+     //    $("button").click(function () {
+     //        // toggle(speed, callback)
+     //        $("div").fadeIn(1000, function () {
+     //            $("div").css("display", "none");
+     //        });
+     //    });
+     //  End FadeOut
+
+     // // Start FadeTo
+     //      $('button').click(function () {
+     //     //   $("div").fadeTo(speed, opacity, function)
+     //          $("div").fadeTo(2000, .5, function () {$("div").fadeOut()});
+     //      });
+     // // End FadeTo          
+
+
+     // //Start SlideDown
+     //  $("button").click(function () {
+     //      $(".div-2").slideDown(2000);
+
+     //  })
+     // //  End SlideDown
+
+
+
+     //      //Start SlideDown
+     //    $(".div-2").css("display", "block");
+     //  $("button").click(function () {
+
+     //          $(".div-2").slideUp(2000);
+
+     //      })
+     //      //  End SlideDown
+
+
+     //   //Start SlideToggle
+     //   $("button").click(function () {
+
+     //       $(".div-2").slideToggle(2000);
+
+     //   })
+     //   //  End SlideToggle
+
+     //    //Start animate
+     //    $("button").click(function () {
+
+     //        $(".div-3").animate({
+     //            width: "600px",
+     //            left: "100px",
+     //            height: "200px",
+     //            opacity: "0.8",
+     //            borderRadius:"50%"
+
+     //        });
+
+     //    })
+     //    //  End animate
+
+
+
+
+     //   //Start stop
+     //  $(".button-2").click(function () {
+     //     //  $(".div-3").stop(clearQueue, goToEnd);
+     //     // if clearQueue == true -> trhe will move to next animate every click button 
+     //     //  if goToEnd ==true -> go to Last Animate and finished by one click
+
+     //      $("div-3").stop();
+     //  });
+
+
+     //   $("button").click(function () {
+     //       $(".div-3").animate({
+     //           width: "600px",
+     //           left: "100px",
+     //           height: "200px",
+     //           opacity: "0.8",
+     //           borderRadius: "50%"
+
+     //       },5000),
+     //            $(".div-3").animate({
+     //                width: "800px",
+     //                left: "150px",
+     //                height: "200px",
+     //                opacity: "0.8",
+     //                borderRadius: "50%"
+
+     //            }, 5000),
+     //                 $(".div-3").animate({
+     //                     width: "1000px",
+     //                     left: "200px",
+     //                     height: "200px",
+     //                     opacity: "0.8",
+     //                     borderRadius: "50%"
+
+     //                 }, 5000);
+
+     //   })
+     //   //  End stop
+
+
+
+
+     //  Start Chain
+     //  $(".div-3").css("background-color", "red")
+     //      .fadeIn(500)
+     //      .fadeOut(500)
+     //      .slideDown(500)
+     //      .slideUp(500)
+     //      .hide(3000).show(2000)
+     //      .animate({
+     //          width: "1000px",
+     //          left: "200px",
+     //          height: "200px",
+     //          opacity: "0.8",
+     //          borderRadius: "50%"
+     //      }, 2000);
+
+     //  End Chain
+
+     //  Set text from input or textarea to chhange p text
+     //  $('button').click(function () {
+
+     //       var t = $('textarea').val();
+     //       $('p').text(t);
+     //  });
+
+
+
+ });
